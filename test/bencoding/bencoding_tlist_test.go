c5e1c74ca1aaa9a6029cc651981df9ac15790e0e
woojiahao <woojiahao1234@gmail.com>
2020-03-29 19:21:57 +0800

    refactor: comment out test cases temporarily

--- test/bencoding/bencoding_tlist_test.go
+++ test/bencoding/bencoding_tlist_test.go
@@ -1,12 +1,10 @@
 package bencoding
 
 import (
-  "fmt"
-  "github.com/woojiahao/torrent.go/internal/bencoding"
   "testing"
 )
 
 func TestTListDecodeValidFormat(t *testing.T) {
-  result := bencoding.Decode("l3:onei8ed3:twoi2eee")
-  fmt.Println(result)
+  //result := bencoding.Decode("l3:onei8ed3:twoi2eee")
+  //fmt.Println(result)
 }
