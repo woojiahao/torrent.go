e186cd0a2ae5c3314a68b74c76eadc01abcd40da
woojiahao <woojiahao1234@gmail.com>
2020-04-07 17:30:01 +0800

    remove: processing.go
    move torrent file processing to download.go

--- internal/torrent/download.go
+++ internal/torrent/download.go
@@ -8,6 +8,43 @@ import (
   "log"
 )
 
+// Parses a torrent file into either a single file torrent or multi file torrent
+func parseTorrentFile(torrentMetadata TDict) (Torrent, bool) {
+  announce, info := ToString(torrentMetadata["announce"]).Value(),
+    ToDict(torrentMetadata["info"])
+
+  isSingle := info["files"] == nil
+
+  name, pieceLength, pieces := ToString(info["name"]).Value(),
+    ToInt(info["piece length"]).Value(),
+    createPieces(ToString(info["pieces"]).Value())
+
+  var torrent Torrent
+
+  if isSingle {
+    torrent = singleFileTorrent{
+      announce,
+      singleFileInfo{
+        length:      ToInt(info["length"]).Value(),
+        name:        name,
+        pieceLength: pieceLength,
+        Pieces:      pieces,
+      },
+    }
+  } else {
+    torrent = multiFileTorrent{
+      announce,
+      multiFileInfo{
+        files:       parseFiles(ToList(info["files"])),
+        name:        name,
+        pieceLength: pieceLength,
+        Pieces:      pieces,
+      },
+    }
+  }
+  return torrent, isSingle
+}
+
 // TODO Retry the download if the original returns a failure
 // Downloads a torrent from the given file path
 func Download(torrentFilename string) {
